e371b7d4cdd60cb9c183e3d3bb0f1003f57056fe
SubbotinYA <yakovsubbotin@gmail.com>
2024-10-14 10:39:45 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+#Количество выполненных ДЗ (запишите значение 12)
+number_of_completed_tasks = 12
+
+#Количество затраченных часов (запишите значение 1.5)
+Number_of_hours_spent = 1.5
+
+#Название курса (запишите значение 'Python')
+CourseName = "'Python'"
+
+#Время на одно задание (вычислить используя 1 и 2 переменные)
+Time_for_one_task = Number_of_hours_spent / number_of_completed_tasks
+print('Название курса:',CourseName, '  Количество выполненных ДЗ:',number_of_completed_tasks, '  Количество затраченных часов:',Number_of_hours_spent, '  Время на одно задание:', Time_for_one_task)
